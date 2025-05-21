3bc6eaae83d87f7d5ef035b52f6e86c3725f95b3
gbitd <gbitdinger@proton.me>
2025-05-21 16:52:26 -0300

    Novo DTO: GET game by id

--- src/main/java/com/gbitd/dslist/controllers/GameController.java
+++ src/main/java/com/gbitd/dslist/controllers/GameController.java
@@ -1,10 +1,12 @@
 package com.gbitd.dslist.controllers;
 
+import com.gbitd.dslist.dto.GameDTO;
 import com.gbitd.dslist.dto.GameMinDTO;
 import com.gbitd.dslist.entities.Game;
 import com.gbitd.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +19,12 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        GameDTO result = gameService.findById(id);
+        return result;
+
+    }
     @GetMapping
     public List<GameMinDTO> findAll(){
         List<GameMinDTO> result = gameService.findAll();
---
+++ src/main/java/com/gbitd/dslist/dto/GameDTO.java
@@ -0,0 +1,103 @@
+package com.gbitd.dslist.dto;
+
+import com.gbitd.dslist.entities.Game;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.beans.BeanUtils;
+
+public class GameDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private Double score;
+    private String imgUrl;
+    private String shortDescription;
+    private String longDescription;
+
+    public GameDTO(){
+
+    }
+
+    public GameDTO(Game entity){
+        //Copia propriedades entre dois objetos que tenham os mesmos nomes de atributos de forma rápida
+        BeanUtils.copyProperties(entity, this);
+
+    }
+
+    // É preciso usar getter e setters para que o copyProperties funcione aparentemente
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+}
