2e048573f00db939f74b3fa61b9e4f6c5b0ff906
gbitd <gbitdinger@proton.me>
2025-05-21 18:22:20 -0300

    Projection: Busca de games por lista

--- src/main/java/com/gbitd/dslist/dto/GameMinDTO.java
+++ src/main/java/com/gbitd/dslist/dto/GameMinDTO.java
@@ -2,6 +2,7 @@ package com.gbitd.dslist.dto;
 
 
 import com.gbitd.dslist.entities.Game;
+import com.gbitd.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -21,7 +22,15 @@ public class GameMinDTO {
         title = entity.getTitle();
         year = entity.getYear();
         imgUrl = entity.getImgUrl();
-        shortDescription = entity.getLongDescription();
+        shortDescription = entity.getShortDescription();
+    }
+
+    public GameMinDTO(GameMinProjection projection) {
+        id = projection.getId();
+        title = projection.getTitle();
+        year = projection.getYear();
+        imgUrl = projection.getImgUrl();
+        shortDescription = projection.getShortDescription();
     }
 
     public Long getId() {
--- src/main/java/com/gbitd/dslist/services/GameService.java
+++ src/main/java/com/gbitd/dslist/services/GameService.java
@@ -3,6 +3,7 @@ package com.gbitd.dslist.services;
 import com.gbitd.dslist.dto.GameDTO;
 import com.gbitd.dslist.dto.GameMinDTO;
 import com.gbitd.dslist.entities.Game;
+import com.gbitd.dslist.projections.GameMinProjection;
 import com.gbitd.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,6 +27,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+
+    }
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
 
